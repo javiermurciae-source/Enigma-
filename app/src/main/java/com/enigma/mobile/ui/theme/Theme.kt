806dfbab8f49565e977787c3103e1b8e2d9d120b
javiermurciae-source <javiermurciae@gmail.com>
2026-08-28 12:31:02 -0500

    feat: add app/src/main/java/com/enigma/mobile/ui/theme/Theme.kt

---
+++ app/src/main/java/com/enigma/mobile/ui/theme/Theme.kt
@@ -0,0 +1,31 @@
+package com.enigma.mobile.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme()
+private val LightColorScheme = lightColorScheme()
+
+@Composable
+fun EnigmaTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
